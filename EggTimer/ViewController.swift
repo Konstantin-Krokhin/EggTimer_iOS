bc157f2dbcf31e6fe6ea6a55d604c17e893eb10d
Konstantin-Krokhin <winpie1@gmail.com>
2023-02-23 18:18:59 +0100

    The egg timer is done with progreess bar and countdown

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,57 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
-
-
+    var player: AVAudioPlayer!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var DoneLabel: UILabel!
+    @IBOutlet weak var CountLabel: UILabel!
+    let eggTimes = ["Soft" : 300, "Medium" : 420, "Hard" : 720]
+    var totalTime = 0
+    var secondPassed = 0
+    
+    var timer = Timer()
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        secondPassed = 0
+        progressBar.progress = 0
+        DoneLabel.text = hardness
+        
+        timer.invalidate()
+        totalTime = eggTimes[hardness]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(update), userInfo: nil, repeats: true)
+    }
+    
+    @objc func update()
+    {
+        if secondPassed < totalTime
+        {
+            secondPassed += 1
+            let percentageProgess : Float = Float(secondPassed) / Float(totalTime)
+            progressBar.progress = percentageProgess
+            CountLabel.text = String(totalTime - secondPassed)
+        }
+        else
+        {
+            timer.invalidate()
+            CountLabel.text = String(totalTime - secondPassed)
+            progressBar.progress = 1
+            DoneLabel.text  = "Done!"
+            playSound()
+        }
+    }
+    
+    func playSound()
+    {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+    
+    
 }
